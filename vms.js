c621a0bffaf3f1db5ffdfecdee26369cdb493824
faru76 <128760930+faru76@users.noreply.github.com>
2024-01-06 18:53:52 +0800

    fix session not creating 2

--- vms.js
+++ vms.js
@@ -18,8 +18,7 @@ app.use(session({
     secret: 'supercalifragilisticexpialidocious', // a random string used for encryption
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
-    cookie: {
-        secure: true, // set to true if your using https
+    cookie: { // set to true if your using https
         maxAge: 1000 * 60 * 60 * 24 // 1 day
     },
 }));
